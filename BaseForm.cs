962a16c580bc985a2b936eddb76b31fd4ebddde9
marcinvi <marcinpaprocki94@gmail.com>
2026-01-12 14:57:05 +0100

    Enable spellcheck across all textboxes

--- BaseForm.cs
+++ BaseForm.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Windows.Forms;
-using System.Collections.Generic;
-using System.Drawing;
 
 namespace Reklamacje_Dane
 {
@@ -30,53 +28,8 @@ namespace Reklamacje_Dane
             // this.Icon = Properties.Resources.top;
 
             // Włącz sprawdzanie pisowni dla wszystkich TextBoxów
-            EnableSpellCheckOnAllTextBoxes();
+            SpellCheckManager.EnableSpellCheckForAllTextBoxes(this);
         }
     
-        /// <summary>
-        /// Włącza sprawdzanie pisowni po polsku dla wszystkich TextBoxów w formularzu
-        /// </summary>
-        private void EnableSpellCheckOnAllTextBoxes()
-        {
-            try
-            {
-                // Włącz sprawdzanie pisowni dla wszystkich kontrolek typu TextBox i RichTextBox
-                foreach (Control control in GetAllControls(this))
-                {
-                    if (control is RichTextBox richTextBox)
-                    {
-                        richTextBox.EnableSpellCheck(true);
-                    }
-                    else if (control is TextBox textBox && !(textBox is SpellCheckTextBox))
-                    {
-                        // Dla zwykłych TextBoxów - bez podkreślania (bo nie obsługują kolorów)
-                        textBox.EnableSpellCheck(false);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Błąd włączania sprawdzania pisowni: {ex.Message}");
-            }
-        }
-
-        /// <summary>
-        /// Rekurencyjnie pobiera wszystkie kontrolki z kontenera
-        /// </summary>
-        private IEnumerable<Control> GetAllControls(Control container)
-        {
-            foreach (Control control in container.Controls)
-            {
-                yield return control;
-
-                if (control.HasChildren)
-                {
-                    foreach (Control child in GetAllControls(control))
-                    {
-                        yield return child;
-                    }
-                }
-            }
-        }
     }
 }
