70ec9c07fa598ac15c7a04467ccd09f5e5a29181
marcinvi <marcinpaprocki94@gmail.com>
2026-01-13 15:30:40 +0100

    h

--- FormPodsumowanieZwrotu.cs
+++ FormPodsumowanieZwrotu.cs
@@ -22,7 +22,7 @@ namespace Reklamacje_Dane
         
 
             // Włącz sprawdzanie pisowni dla wszystkich TextBoxów
-            EnableSpellCheckOnAllTextBoxes();
+        
         }
 
         private async void FormPodsumowanieZwrotu_Load(object sender, EventArgs e)
@@ -160,47 +160,11 @@ namespace Reklamacje_Dane
         /// <summary>
         /// Włącza sprawdzanie pisowni po polsku dla wszystkich TextBoxów w formularzu
         /// </summary>
-        private void EnableSpellCheckOnAllTextBoxes()
-        {
-            try
-            {
-                // Włącz sprawdzanie pisowni dla wszystkich kontrolek typu TextBox i RichTextBox
-                foreach (Control control in GetAllControls(this))
-                {
-                    if (control is RichTextBox richTextBox)
-                    {
-                        richTextBox.EnableSpellCheck(true);
-                    }
-                    else if (control is TextBox textBox && !(textBox is SpellCheckTextBox))
-                    {
-                        // Dla zwykłych TextBoxów - bez podkreślania (bo nie obsługują kolorów)
-                        textBox.EnableSpellCheck(false);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Błąd włączania sprawdzania pisowni: {ex.Message}");
-            }
-        }
+        
 
         /// <summary>
         /// Rekurencyjnie pobiera wszystkie kontrolki z kontenera
         /// </summary>
-        private IEnumerable<Control> GetAllControls(Control container)
-        {
-            foreach (Control control in container.Controls)
-            {
-                yield return control;
-
-                if (control.HasChildren)
-                {
-                    foreach (Control child in GetAllControls(control))
-                    {
-                        yield return child;
-                    }
-                }
-            }
-        }
+       
 }
 }
